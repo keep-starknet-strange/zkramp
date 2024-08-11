cbea89de56de46b628d3ef744f0da8455161cba0
BlackStarkGoku <165695008+BlackStarkGoku@users.noreply.github.com>
2024-08-11 14:15:40 +0200

    Feature: Add Revolut register page for client
    - Add page without logic of Revolut register page

--- client/src/state/application.ts
+++ client/src/state/application.ts
@@ -22,7 +22,7 @@ interface Actions {
 
 export const createApplicationSlice: StateCreator<StoreState, [['zustand/immer', never]], [], ApplicationSlice> = (
   set,
-  get,
+  get
 ) => ({
   modal: null,
 
