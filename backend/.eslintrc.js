09c8f999671bd6f857fa52ebc114596c3a937fdb
Uğur Eren <contact@ugureren.net>
2024-09-04 18:23:20 +0300

    ci: backend CI job

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -5,59 +5,56 @@ module.exports = {
     es6: true,
     node: true,
   },
-  plugins: ["import", "simple-import-sort", "unused-imports"],
-  extends: ["eslint:recommended"],
+  plugins: ['import', 'simple-import-sort', 'unused-imports'],
+  extends: ['eslint:recommended'],
   parserOptions: {
     ecmaVersion: 2020,
-    sourceType: "module",
+    sourceType: 'module',
   },
   rules: {
-    "prettier/prettier": [
-      "error",
+    'prettier/prettier': [
+      'error',
       {
         semi: false,
         singleQuote: true,
         printWidth: 120,
       },
     ],
-    "import/no-unused-modules": ["error", { unusedExports: true }],
-    "object-shorthand": ["error", "always"],
-    "unused-imports/no-unused-imports": "error",
-    "simple-import-sort/imports": "error",
-    "simple-import-sort/exports": "error",
+    'import/no-unused-modules': ['error', { unusedExports: true }],
+    'object-shorthand': ['error', 'always'],
+    'unused-imports/no-unused-imports': 'error',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
   },
   overrides: [
     {
-      files: ["*.ts"],
-      parser: "@typescript-eslint/parser",
-      plugins: ["@typescript-eslint/eslint-plugin"],
-      extends: [
-        "plugin:@typescript-eslint/recommended",
-        "plugin:import/typescript",
-      ],
+      files: ['*.ts'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint/eslint-plugin'],
+      extends: ['plugin:@typescript-eslint/recommended', 'plugin:import/typescript'],
       settings: {
-        "import/parsers": {
-          "@typescript-eslint/parser": [".ts"],
+        'import/parsers': {
+          '@typescript-eslint/parser': ['.ts'],
         },
-        "import/resolver": {
+        'import/resolver': {
           typescript: {
             alwaysTryTypes: true,
           },
         },
       },
       rules: {
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/ban-ts-comment": "off",
-        "@typescript-eslint/ban-ts-ignore": "off",
-        "@typescript-eslint/explicit-module-boundary-types": "off",
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "@typescript-eslint/no-restricted-imports": ["error"],
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/ban-ts-ignore': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-restricted-imports': ['error'],
       },
     },
     {
-      files: ["*"],
-      plugins: ["prettier"],
-      extends: ["plugin:prettier/recommended"],
+      files: ['*'],
+      plugins: ['prettier'],
+      extends: ['plugin:prettier/recommended'],
     },
   ],
-};
+}
