690231192afb218762b5bc042a91849b2123f475
Uğur Eren <contact@ugureren.net>
2024-09-04 18:10:20 +0300

    fix: use enum for network

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,10 +1,12 @@
-import { bigint, pgTable, text, timestamp } from 'drizzle-orm/pg-core'
+import { bigint, pgEnum, pgTable, text, timestamp } from 'drizzle-orm/pg-core'
+
+export const networkEnum = pgEnum('network_type', ['mainnet', 'sepolia'])
 
 const indexerCommonSchema = {
   cursor: bigint('_cursor', { mode: 'number' }),
   createdAt: timestamp('created_at', { mode: 'date', withTimezone: false }),
 
-  network: text('network'),
+  network: networkEnum('network'),
   blockHash: text('block_hash'),
   blockNumber: bigint('block_number', { mode: 'number' }),
   blockTimestamp: timestamp('block_timestamp', {
