2ac4d20993d32d5e846413a79650ffb6f2737991
0xChqrles <clanier.dev@gmail.com>
2024-09-04 16:16:39 +0200

    fix proof generation modal

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,6 +1,8 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { PrimaryButton } from 'src/components/Button'
 import { Column, Row } from 'src/components/Flex'
+import GenerateProofModal from 'src/components/GenerateProofModal'
+import { useProofGenerationModal } from 'src/hooks/useModal'
 import { ThemedText } from 'src/theme/components'
 import { LockClosed, LockOpen } from 'src/theme/components/icons'
 import { styled, useTheme } from 'styled-components'
@@ -47,66 +49,67 @@ const ProofCard = styled(Row)`
 `
 
 export default function RegisterPage() {
+  // theme
   const theme = useTheme()
-  const [revtag, setRevtag] = useState('')
-  const [generatingProof, setGeneratingProof] = useState(false)
-  const [proven, setProven] = useState(false)
 
-  useEffect(() => {
-    if (generatingProof) {
-      setTimeout(() => {
-        setProven(true)
-        setGeneratingProof(false)
-      }, 5_000)
-    }
-  }, [generatingProof])
+  // modal
+  const [, toggleProofGenerationModal] = useProofGenerationModal()
+
+  const [revtag, setRevtag] = useState('')
+  const [proven] = useState(false)
 
   return (
-    <Content gap={12}>
-      <Headline>
-        <ThemedText.HeadlineLarge>Register</ThemedText.HeadlineLarge>
-      </Headline>
+    <>
+      <Content gap={12}>
+        <Headline>
+          <ThemedText.HeadlineLarge>Register</ThemedText.HeadlineLarge>
+        </Headline>
 
-      {!revtag && (
-        <>
-          <ContentCard>
-            <NoDataCard>
-              <ThemedText.BodyPrimary fontWeight={500}>No data detected</ThemedText.BodyPrimary>
-            </NoDataCard>
-          </ContentCard>
+        {!revtag && (
+          <>
+            <ContentCard>
+              <NoDataCard>
+                <ThemedText.BodyPrimary fontWeight={500}>No data detected</ThemedText.BodyPrimary>
+              </NoDataCard>
+            </ContentCard>
 
-          <PrimaryButton onClick={() => setRevtag('chqrlesjuzw')}>Open sidebar</PrimaryButton>
-        </>
-      )}
+            <PrimaryButton onClick={() => setRevtag('chqrlesjuzw')}>Open sidebar</PrimaryButton>
+          </>
+        )}
 
-      {revtag && (
-        <>
-          <ContentCard>
-            <RevtagCard>
-              <ThemedText.BodyPrimary>Revtag:</ThemedText.BodyPrimary>
-              <ThemedText.BodyPrimary fontWeight={500}>{revtag}</ThemedText.BodyPrimary>
-            </RevtagCard>
+        {revtag && (
+          <>
+            <ContentCard>
+              <RevtagCard>
+                <ThemedText.BodyPrimary>Revtag:</ThemedText.BodyPrimary>
+                <ThemedText.BodyPrimary fontWeight={500}>{revtag}</ThemedText.BodyPrimary>
+              </RevtagCard>
 
-            <ProofCard>
-              {proven ? (
-                <LockClosed width={18} height={18} color={theme.green} />
-              ) : (
-                <LockOpen width={18} height={18} color={theme.neutral2} />
-              )}
+              <ProofCard>
+                {proven ? (
+                  <LockClosed width={18} height={18} color={theme.green} />
+                ) : (
+                  <LockOpen width={18} height={18} color={theme.neutral2} />
+                )}
 
-              {proven ? (
-                <ThemedText.BodyPrimary>Proved</ThemedText.BodyPrimary>
-              ) : (
-                <ThemedText.BodySecondary fontWeight={500}>Unproved</ThemedText.BodySecondary>
-              )}
-            </ProofCard>
-          </ContentCard>
+                {proven ? (
+                  <ThemedText.BodyPrimary>Proved</ThemedText.BodyPrimary>
+                ) : (
+                  <ThemedText.BodySecondary>Unproved</ThemedText.BodySecondary>
+                )}
+              </ProofCard>
+            </ContentCard>
 
-          <PrimaryButton onClick={() => !proven && setGeneratingProof(true)}>
-            <ThemedText.BodyPrimary>{proven ? 'Complete registration' : 'Generate proof'}</ThemedText.BodyPrimary>
-          </PrimaryButton>
-        </>
-      )}
-    </Content>
+            {proven ? (
+              <PrimaryButton>Complete registration</PrimaryButton>
+            ) : (
+              <PrimaryButton onClick={toggleProofGenerationModal}>Generate proof</PrimaryButton>
+            )}
+          </>
+        )}
+      </Content>
+
+      <GenerateProofModal />
+    </>
   )
 }
