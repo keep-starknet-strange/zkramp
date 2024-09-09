64d2b9a6986ffb830ec5d3818d3d6894afadf937
Uğur Eren <contact@ugureren.net>
2024-09-09 15:03:29 +0300

    feat: Apibara indexers (#47)
    * feat: Init apibara indexer repo
    
    * fix: rename indexer to indexers
    
    * fix: use docker compose instead of dockerfile
    
    * feat: locked & unlocked indexers
    
    * fix: remove init.sql file
    
    * style: format with deno fmt
    
    * ci: indexers CI job
    
    * fix: move eventId to indexer
    
    * fix: deno fmt format error
    
    * fix: remove .env file from indexers

---
+++ indexers/src/utils/helpers.ts
@@ -0,0 +1,22 @@
+import { apibara } from './deps.ts'
+
+export const getCommonValues = (
+  header: apibara.BlockHeader,
+  event: apibara.Event,
+  transaction: apibara.Transaction,
+) => {
+  const { blockNumber, blockHash, timestamp } = header
+
+  const transactionHash = transaction.meta.hash
+  const IndexInBlock = (transaction.meta.transactionIndex ?? 0) * 1_000 + (event.index ?? 0)
+
+  return {
+    created_at: new Date().toISOString(),
+    network: 'mainnet',
+    block_hash: blockHash,
+    block_number: +(blockNumber ?? 0),
+    block_timestamp: timestamp,
+    transaction_hash: transactionHash,
+    index_in_block: IndexInBlock,
+  }
+}
